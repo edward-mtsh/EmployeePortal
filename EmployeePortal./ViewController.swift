971016577aafe9544327b37b7791096c739cba76
Edward Mtshweni <109082496+zanelemtsh@users.noreply.github.com>
2024-02-04 12:39:27 +0200

    Initial Commit

---
+++ EmployeePortal./ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmployeePortal.
+//
+//  Created by Zanele Mtshweni on 2024/02/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
